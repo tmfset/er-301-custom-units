c477edb3090f710d43c7c6b44e4edfd9293db214
Tom Fiset <tomfiset@curalate.com>
2019-11-21 00:24:31 -0500

    Be more organized

--- Synth/MultiVoice.lua
+++ Synth/MultiVoice.lua
@@ -19,113 +19,258 @@ function MultiVoice:init(args)
   Unit.init(self, args)
 end
 
+function MultiVoice:createControls()
+  return {
+    f0           = self:createObject("GainBias", "f0"),
+    f0Range      = self:createObject("MinMax", "f0Range"),
+    detune       = self:createObject("ConstantOffset", "detune"),
+    detuneRange  = self:createObject("MinMax", "detuneRange"),
+    cutoff       = self:createObject("GainBias", "cutoff"),
+    cutoffRange  = self:createObject("MinMax", "cutoffRange"),
+    res          = self:createObject("GainBias", "res"),
+    resRange     = self:createObject("MinMax", "resRange"),
+    clipper      = self:createObject("Clipper", "clipper"),
+    fenv         = self:createObject("GainBias", "fenv"),
+    fenvRange    = self:createObject("MinMax", "fenvRange"),
+    attack       = self:createObject("GainBias", "attack"),
+    attackRange  = self:createObject("MinMax", "attackRange"),
+    decay        = self:createObject("GainBias", "decay"),
+    decayRange   = self:createObject("MinMax", "decayRange"),
+    sustain      = self:createObject("GainBias", "sustain"),
+    sustainRange = self:createObject("MinMax", "sustainRange"),
+    release      = self:createObject("GainBias", "release"),
+    releaseRange = self:createObject("MinMax", "releaseRange")
+  }
+end
+
+function MultiVoice:connectControls(controls)
+  connect(controls.f0, "Out", controls.f0Range, "In")
+  connect(controls.detune, "Out", controls.detuneRange, "In")
+  connect(controls.cutoff, "Out", controls.cutoffRange, "In")
+  connect(controls.res, "Out", controls.clipper, "In")
+  connect(controls.clipper, "Out", controls.resRange, "In")
+  connect(controls.fenv, "Out", controls.fenvRange, "In")
+  connect(controls.attack, "Out", controls.attackRange, "In")
+  connect(controls.decay, "Out", controls.decayRange, "In")
+  connect(controls.sustain, "Out", controls.sustainRange, "In")
+  connect(controls.release, "Out", controls.releaseRange, "In")
+
+  controls.clipper:setMaximum(0.999)
+  controls.clipper:setMinimum(0)
+
+  self:createMonoBranch("f0", controls.f0, "In", controls.f0, "Out")
+  self:createMonoBranch("detune", controls.detune, "In", controls.detune, "Out")
+  self:createMonoBranch("cutoff", controls.cutoff, "In", controls.cutoff, "Out")
+  self:createMonoBranch("Q", controls.res, "In", controls.res, "Out")
+  self:createMonoBranch("fenv", controls.fenv, "In", controls.fenv, "Out")
+  self:createMonoBranch("attack", controls.attack, "In", controls.attack, "Out")
+  self:createMonoBranch("decay", controls.decay, "In", controls.decay, "Out")
+  self:createMonoBranch("sustain", controls.sustain, "In", controls.sustain, "Out")
+  self:createMonoBranch("release", controls.release, "In", controls.release, "Out")
+end
+
+function MultiVoice:createVoice(i)
+  return {
+    index     = i,
+    tune      = self:createObject("ConstantOffset", "tune"..i),
+    tuneRange = self:createObject("MinMax", "tuneRange"..i),
+    oscA      = self:createObject("SawtoothOscillator", "oscA"..i),
+    oscB      = self:createObject("SawtoothOscillator", "oscB"..i),
+    detuneSum = self:createObject("Sum", "detuneSum"..i),
+    mixer     = self:createObject("Mixer", "mixer"..i, 2),
+    gate      = self:createObject("Comparator", "gate"..i),
+    adsr      = self:createObject("ADSR", "adsr"..i),
+    vca       = self:createObject("Multiply", "vca"..i),
+    fenvVca   = self:createObject("Multiply", "fenvVca"..i),
+    filter    = self:createObject("StereoLadderFilter", "filter"..i)
+  }
+end
+
+function MultiVoice:connectVoice(voice, controls)
+  connect(controls.f0, "Out", voice.oscA, "Fundamental")
+  connect(controls.f0, "Out", voice.oscB, "Fundamental")
+  connect(voice.tune, "Out", voice.tuneRange, "In")
+  connect(voice.tune, "Out", voice.detuneSum, "Left")
+  connect(controls.detune, "Out", voice.detuneSum, "Right")
+  connect(voice.tune, "Out", voice.oscA, "V/Oct")
+  connect(voice.detuneSum, "Out", voice.oscB, "V/Oct")
+
+  connect(voice.oscA, "Out", voice.mixer, "In1")
+  connect(voice.oscB, "Out", voice.mixer, "In2")
+  voice.mixer:hardSet("Gain1", 0.5)
+  voice.mixer:hardSet("Gain2", 0.5)
+
+  connect(voice.gate, "Out", voice.adsr, "Gate")
+  voice.gate:setGateMode()
+
+  connect(voice.adsr, "Out", voice.fenvVca, "Left")
+  connect(voice.adsr, "Out", voice.vca, "Left")
+  connect(voice.mixer, "Out", voice.vca, "Right")
+  connect(controls.attack, "Out", voice.adsr, "Attack")
+  connect(controls.decay, "Out", voice.adsr, "Decay")
+  connect(controls.sustain, "Out", voice.adsr, "Sustain")
+  connect(controls.release, "Out", voice.adsr, "Release")
+
+  connect(controls.fenv, "Out", voice.fenvVca, "Right")
+  connect(voice.fenvVca, "Out", voice.filter, "V/Oct")
+  connect(controls.cutoff, "Out", voice.filter, "Fundamental")
+  connect(controls.clipper, "Out", voice.filter, "Resonance")
+  connect(voice.vca, "Out", voice.filter, "Left In")
+
+  self:createMonoBranch("gate"..voice.index, voice.gate, "In", voice.gate, "Out")
+  self:createMonoBranch("tune"..voice.index, voice.tune, "In", voice.tune, "Out")
+
+  return voice.filter, "Left Out"
+end
+
 function MultiVoice:onLoadGraph(channelCount)
   local voiceMixer = self:createObject("Mixer", "voiceMixer", self.voiceCount)
   connect(voiceMixer, "Out", self, "Out1")
 
-  local f0 = self:createObject("GainBias", "f0")
-  local f0Range = self:createObject("MinMax", "f0Range")
-  connect(f0, "Out", f0Range, "In")
-  self:createMonoBranch("f0", f0, "In", f0, "Out")
-
-  local detune = self:createObject("ConstantOffset", "detune")
-  local detuneRange = self:createObject("MinMax", "detuneRange")
-  connect(detune, "Out", detuneRange, "In")
-  self:createMonoBranch("detune", detune, "In", detune, "Out")
-
-  local cutoff = self:createObject("GainBias", "cutoff")
-  local cutoffRange = self:createObject("MinMax", "cutoffRange")
-  connect(cutoff, "Out", cutoffRange, "In")
-  self:createMonoBranch("cutoff", cutoff, "In", cutoff, "Out")
-
-  local res = self:createObject("GainBias", "res")
-  local resRange = self:createObject("MinMax", "resRange")
-  local clipper = self:createObject("Clipper", "clipper")
-  clipper:setMaximum(0.999)
-  clipper:setMinimum(0)
-  connect(res, "Out", clipper, "In")
-  connect(clipper, "Out", resRange, "In")
-  self:createMonoBranch("Q", res, "In", res, "Out")
-
-  local fenv = self:createObject("GainBias", "fenv")
-  local fenvRange = self:createObject("MinMax", "fenvRange")
-  connect(fenv, "Out", fenvRange, "In")
-  self:createMonoBranch("fenv", fenv, "In", fenv, "Out")
-
-  local attack = self:createObject("GainBias", "attack")
-  local attackRange = self:createObject("MinMax", "attackRange")
-  connect(attack, "Out", attackRange, "In")
-  self:createMonoBranch("attack", attack, "In", attack, "Out")
-
-  local decay = self:createObject("GainBias", "decay")
-  local decayRange = self:createObject("MinMax", "decayRange")
-  connect(decay, "Out", decayRange, "In")
-  self:createMonoBranch("decay", decay, "In", decay, "Out")
-
-  local sustain = self:createObject("GainBias", "sustain")
-  local sustainRange = self:createObject("MinMax", "sustainRange")
-  connect(sustain, "Out", sustainRange, "In")
-  self:createMonoBranch("sustain", sustain, "In", sustain, "Out")
-
-  local release = self:createObject("GainBias", "release")
-  local releaseRange = self:createObject("MinMax", "releaseRange")
-  connect(release, "Out", releaseRange, "In")
-  self:createMonoBranch("release", release, "In", release, "Out")
+  local controls = self:createControls()
+  self:connectControls(controls)
 
   for i = 1, self.voiceCount do
-    local tune = self:createObject("ConstantOffset", "tune"..i)
-    local tuneRange = self:createObject("MinMax", "tuneRange"..i)
-    connect(tune, "Out", tuneRange, "In")
-    self:createMonoBranch("tune"..i, tune, "In", tune, "Out")
+    local voice = self:createVoice(i)
+    local out, key = self:connectVoice(voice, controls)
 
-    local oscA = self:createObject("SawtoothOscillator", "oscA"..i)
-    local oscB = self:createObject("SawtoothOscillator", "oscB"..i)
-    local detuneSum = self:createObject("Sum", "detuneSum"..i)
-
-    connect(tune, "Out", detuneSum, "Left")
-    connect(detune, "Out", detuneSum, "Right")
-
-    connect(f0, "Out", oscA, "Fundamental")
-    connect(tune, "Out", oscA, "V/Oct")
-    connect(f0, "Out", oscB, "Fundamental")
-    connect(detuneSum, "Out", oscB, "V/Oct")
-
-    local mixer = self:createObject("Mixer", "mixer"..i, 2)
-    mixer:hardSet("Gain1", 0.5)
-    mixer:hardSet("Gain2", 0.5)
-
-    connect(oscA, "Out", mixer, "In1")
-    connect(oscB, "Out", mixer, "In2")
-
-    local gate = self:createObject("Comparator", "gate"..i)
-    gate:setGateMode()
-    self:createMonoBranch("gate"..i, gate, "In", gate, "Out")
-
-    local adsr = self:createObject("ADSR", "adsr"..i)
-    local vca = self:createObject("Multiply", "vca"..i)
-
-    connect(gate, "Out", adsr, "Gate")
-    connect(attack, "Out", adsr, "Attack")
-    connect(decay, "Out", adsr, "Decay")
-    connect(sustain, "Out", adsr, "Sustain")
-    connect(release, "Out", adsr, "Release")
-
-    connect(adsr, "Out", vca, "Left")
-    connect(mixer, "Out", vca, "Right")
-
-    local fenvVca = self:createObject("Multiply", "fenvVca"..i)
-    local filter = self:createObject("StereoLadderFilter", "filter"..i)
-
-    connect(adsr, "Out", fenvVca, "Left")
-    connect(fenv, "Out", fenvVca, "Right")
-    connect(fenvVca, "Out", filter, "V/Oct")
-    connect(cutoff, "Out", filter, "Fundamental")
-    connect(clipper, "Out", filter, "Resonance")
-
-    connect(vca, "Out", filter, "Left In")
-    connect(filter, "Left Out", voiceMixer, "In"..i)
+    connect(out, key, voiceMixer, "In"..i)
     voiceMixer:hardSet("Gain"..i, 1 / self.voiceCount)
   end
+end
+
+function MultiVoice:setVoiceViews(i, objects, branches, controls, views)
+  controls["gate"..i] = Gate {
+    button      = "gate"..i,
+    description = "Gate "..i,
+    branch      = branches["gate"..i],
+    comparator  = objects["gate"..i]
+  }
+  views.expanded[#views.expanded + 1] = "gate"..i
+
+  controls["tune"..i] = Pitch {
+    button      = "V/oct"..i,
+    description = "V/oct "..i,
+    branch      = branches["tune"..i],
+    offset      = objects["tune"..i],
+    range       = objects["tuneRange"..i]
+  }
+  views.expanded[#views.expanded + 1] = "tune"..i
+end
+
+function MultiVoice:setPitchViews(objects, branches, controls, views)
+  controls.freq = GainBias {
+    button      = "f0",
+    description = "Fundamental",
+    branch      = branches.f0,
+    gainbias    = objects.f0,
+    range       = objects.f0Range,
+    biasMap     = Encoder.getMap("oscFreq"),
+    biasUnits   = app.unitHertz,
+    initialBias = 27.5,
+    gainMap     = Encoder.getMap("freqGain"),
+    scaling     = app.octaveScaling
+  }
+  views.expanded[#views.expanded + 1] = "freq"
+
+  controls.detune = Pitch {
+    button      = "detune",
+    description = "Detune",
+    branch      = branches.detune,
+    offset      = objects.detune,
+    range       = objects.detuneRange
+  }
+  views.expanded[#views.expanded + 1] = "detune"
+end
+
+function MultiVoice:setFilterViews(objects, branches, controls, views)
+  controls.cutoff = GainBias {
+    button      = "cutoff",
+    branch      = branches.cutoff,
+    description = "Filter Cutoff",
+    gainbias    = objects.cutoff,
+    range       = objects.cutoffRange,
+    biasMap     = Encoder.getMap("filterFreq"),
+    biasUnits   = app.unitHertz,
+    initialBias = 440,
+    gainMap     = Encoder.getMap("freqGain"),
+    scaling     = app.octaveScaling
+  }
+  views.expanded[#views.expanded + 1] = "cutoff"
+
+  controls.resonance = GainBias {
+    button      = "Q",
+    branch      = branches.Q,
+    description = "Resonance",
+    gainbias    = objects.res,
+    range       = objects.resRange,
+    biasMap     = Encoder.getMap("unit"),
+    biasUnits   = app.unitNone,
+    initialBias = 0.25,
+    gainMap     = Encoder.getMap("[-10,10]")
+  }
+  views.expanded[#views.expanded + 1] = "resonance"
+
+  controls.fenv = GainBias {
+    button      = "fenv",
+    description = "Filter Env Amount",
+    branch      = branches.fenv,
+    gainbias    = objects.fenv,
+    range       = objects.fenvRange,
+    biasMap     = Encoder.getMap("[-1,1]"),
+    initialBias = 0.5
+  }
+  views.expanded[#views.expanded + 1] = "fenv"
+end
+
+function MultiVoice:setAdsrViews(objects, branches, controls, views)
+  controls.attack = GainBias {
+    button      = "A",
+    branch      = branches.attack,
+    description = "Attack",
+    gainbias    = objects.attack,
+    range       = objects.attackRange,
+    biasMap     = Encoder.getMap("ADSR"),
+    biasUnits   = app.unitSecs,
+    initialBias = 0.050
+  }
+  views.expanded[#views.expanded + 1] = "attack"
+
+  controls.decay = GainBias {
+    button      = "D",
+    branch      = branches.decay,
+    description = "Decay",
+    gainbias    = objects.decay,
+    range       = objects.decayRange,
+    biasMap     = Encoder.getMap("ADSR"),
+    biasUnits   = app.unitSecs,
+    initialBias = 0.050
+  }
+  views.expanded[#views.expanded + 1] = "decay"
+
+  controls.sustain = GainBias {
+    button      = "S",
+    branch      = branches.sustain,
+    description = "Sustain",
+    gainbias    = objects.sustain,
+    range       = objects.sustainRange,
+    biasMap     = Encoder.getMap("unit"),
+    biasUnits   = app.unitNone,
+    initialBias = 1
+  }
+  views.expanded[#views.expanded + 1] = "sustain"
+
+  controls.release = GainBias {
+    button      = "R",
+    branch      = branches.release,
+    description = "Release",
+    gainbias    = objects.release,
+    range       = objects.releaseRange,
+    biasMap     = Encoder.getMap("ADSR"),
+    biasUnits   = app.unitSecs,
+    initialBias = 0.100
+  }
+  views.expanded[#views.expanded + 1] = "release"
 end
 
 function MultiVoice:onLoadViews(objects, branches)
@@ -138,143 +283,12 @@ function MultiVoice:onLoadViews(objects, branches)
   }
 
   for i = 1, self.voiceCount do
-    controls["gate"..i] = Gate {
-      button = "gate"..i,
-      description = "Gate "..i,
-      branch = branches["gate"..i],
-      comparator = objects["gate"..i]
-    }
-
-    controls["tune"..i] = Pitch {
-      button = "V/oct"..i,
-      description = "V/oct "..i,
-      branch = branches["tune"..i],
-      offset = objects["tune"..i],
-      range = objects["tuneRange"..i]
-    }
-
-    views.expanded[((i - 1) * 2) + 1] = "gate"..i
-    views.expanded[((i - 1) * 2) + 2] = "tune"..i
+    self:setVoiceViews(i, objects, branches, controls, views)
   end
 
-  local controlCount = (self.voiceCount * 2) + 1
-
-  controls.freq = GainBias {
-    button = "f0",
-    description = "Fundamental",
-    branch = branches.f0,
-    gainbias = objects.f0,
-    range = objects.f0Range,
-    biasMap = Encoder.getMap("oscFreq"),
-    biasUnits = app.unitHertz,
-    initialBias = 27.5,
-    gainMap = Encoder.getMap("freqGain"),
-    scaling = app.octaveScaling
-  }
-  views.expanded[controlCount] = "freq"
-  controlCount = controlCount + 1
-
-  controls.detune = Pitch {
-    button = "detune",
-    description = "Detune",
-    branch = branches.detune,
-    offset = objects.detune,
-    range = objects.detuneRange
-  }
-  views.expanded[controlCount] = "detune"
-  controlCount = controlCount + 1
-
-  controls.cutoff = GainBias {
-    button = "cutoff",
-    branch = branches.cutoff,
-    description = "Filter Cutoff",
-    gainbias = objects.cutoff,
-    range = objects.cutoffRange,
-    biasMap = Encoder.getMap("filterFreq"),
-    biasUnits = app.unitHertz,
-    initialBias = 440,
-    gainMap = Encoder.getMap("freqGain"),
-    scaling = app.octaveScaling
-  }
-  views.expanded[controlCount] = "cutoff"
-  controlCount = controlCount + 1
-
-  controls.resonance = GainBias {
-    button = "Q",
-    branch = branches.Q,
-    description = "Resonance",
-    gainbias = objects.res,
-    range = objects.resRange,
-    biasMap = Encoder.getMap("unit"),
-    biasUnits = app.unitNone,
-    initialBias = 0.25,
-    gainMap = Encoder.getMap("[-10,10]")
-  }
-  views.expanded[controlCount] = "resonance"
-  controlCount = controlCount + 1
-
-  controls.fenv = GainBias {
-    button = "fenv",
-    description = "Filter Env Amount",
-    branch = branches.fenv,
-    gainbias = objects.fenv,
-    range = objects.fenvRange,
-    biasMap = Encoder.getMap("[-1,1]"),
-    initialBias = 0.5
-  }
-  views.expanded[controlCount] = "fenv"
-  controlCount = controlCount + 1
-
-  controls.attack = GainBias {
-    button = "A",
-    branch = branches.attack,
-    description = "Attack",
-    gainbias = objects.attack,
-    range = objects.attackRange,
-    biasMap = Encoder.getMap("ADSR"),
-    biasUnits = app.unitSecs,
-    initialBias = 0.050
-  }
-  views.expanded[controlCount] = "attack"
-  controlCount = controlCount + 1
-
-  controls.decay = GainBias {
-    button = "D",
-    branch = branches.decay,
-    description = "Decay",
-    gainbias = objects.decay,
-    range = objects.decayRange,
-    biasMap = Encoder.getMap("ADSR"),
-    biasUnits = app.unitSecs,
-    initialBias = 0.050
-  }
-  views.expanded[controlCount] = "decay"
-  controlCount = controlCount + 1
-
-  controls.sustain = GainBias {
-    button = "S",
-    branch = branches.sustain,
-    description = "Sustain",
-    gainbias = objects.sustain,
-    range = objects.sustainRange,
-    biasMap = Encoder.getMap("unit"),
-    biasUnits = app.unitNone,
-    initialBias = 1
-  }
-  views.expanded[controlCount] = "sustain"
-  controlCount = controlCount + 1
-
-  controls.release = GainBias {
-    button = "R",
-    branch = branches.release,
-    description = "Release",
-    gainbias = objects.release,
-    range = objects.releaseRange,
-    biasMap = Encoder.getMap("ADSR"),
-    biasUnits = app.unitSecs,
-    initialBias = 0.100
-  }
-  views.expanded[controlCount] = "release"
+  self:setPitchViews(objects, branches, controls, views)
+  self:setFilterViews(objects, branches, controls, views)
+  self:setAdsrViews(objects, branches, controls, views)
 
   return controls, views
 end
