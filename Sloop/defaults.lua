c2d1ce9262da011349a69135805dbb96e844c6fa
Tom Fiset <tomfiset@curalate.com>
2020-06-11 20:43:42 -0400

    Simplify

--- Sloop/defaults.lua
+++ Sloop/defaults.lua
@@ -1,17 +1,4 @@
--- A default configuration for Sloop to start in manual mode.
-local defaultManual = {
-  maxSteps                = 64,
-  initialSteps            = 4,
-  initialFeedback         = 1,
-  startEngaged            = 1, -- yes
-  defaultMode             = 2, -- manual
-  defaultLengthMode       = 2, -- free
-  defaultResetOnDisengage = 2, -- no
-  defaultResetOnRecord    = 2  -- no
-}
-
--- A default configuration for Sloop to start in continuous mode.
-local defaultContinuous = {
+return {
   maxSteps                = 64,
   initialSteps            = 4,
   initialFeedback         = 1,
@@ -25,5 +12,3 @@ local defaultContinuous = {
   defaultResetOnDisengage = 2, -- no
   defaultResetOnRecord    = 2  -- no
 }
-
-return defaultContinuous
