0103afccd33aaca3c83ba56feaf8e9b32f0f37f1
Tom Fiset <tomj.fiset@gmail.com>
2020-11-02 00:59:05 -0500

    Strike LPG (#13)
    * Initial
    
    * Working strike
    
    * Loop WIP
    
    * Add README, remove extras
    
    * Update Readme

---
+++ Strike/defaults.lua
@@ -0,0 +1,13 @@
+return {
+  initialLift     = 0.5,
+  initialPeak     = 13290,
+  initialQ        = 0,
+
+  initialTime     = 0.5,
+  initialAttack   = 0.2,
+  initialDecay    = 2,
+
+  initialCurve    = 1,
+  initialCurveIn  = 0,
+  initialCurveOut = 1
+}
---
+++ Strike/init.lua
@@ -0,0 +1,9 @@
+return {
+  title   = "Strike",
+  contact = "tomj.fiset@gmail.com",
+  keyword = "lpg",
+  units   = {
+    { category = "Filtering" },
+    { title = "LPG", moduleName = "Strike", keywords = "lpg" },
+  }
+}
