66c9f2c184f5fa2a9ffc7e2b35f569bbd5776a5d
Tom Fiset <tomj.fiset@gmail.com>
2020-12-22 14:35:34 -0500

    Arc initial commit (#15)

---
+++ Arc/defaults.lua
@@ -0,0 +1,11 @@
+return {
+  initialLift     = 1,
+
+  initialTime     = 0.5,
+  initialAttack   = 0.2,
+  initialDecay    = 2,
+
+  initialCurve    = 1,
+  initialCurveIn  = 0,
+  initialCurveOut = 1
+}
