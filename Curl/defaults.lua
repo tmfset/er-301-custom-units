986306cd6d2ce9c35f5fe5422ae96a481f576c76
Tom Fiset <tomj.fiset@gmail.com>
2020-12-21 22:14:20 -0500

    init (#14)

---
+++ Curl/defaults.lua
@@ -0,0 +1,12 @@
+return {
+  -- How many reflections to do before hard clipping.
+  depth = 10,
+
+  -- Inital parameter values.
+  initial = {
+    gain    = 0,
+    limit   = 1,
+    reflect = -1,
+    wet     = 1
+  }
+}
---
+++ Curl/init.lua
@@ -0,0 +1,9 @@
+return {
+  title   = "Curl",
+  contact = "tomj.fiset@gmail.com",
+  keyword = "wave folder",
+  units   = {
+    { category = "Mapping and Control" },
+    { title = "Curl", moduleName = "Curl", keywords = "wave folder" }
+  }
+}
