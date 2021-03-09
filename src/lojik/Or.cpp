b64d91f5d16c2a22c5892d14560064c358877ff9
Tom Fiset <tomfiset@curalate.com>
2021-03-09 13:24:52 -0500

    Fix bug in or and and

--- src/lojik/Or.cpp
+++ src/lojik/Or.cpp
@@ -18,15 +18,19 @@ namespace lojik {
     float *right = mRight.buffer();
     float *out   = mOut.buffer();
 
-    float32x4_t zero = vdupq_n_f32(0.0f);
+    uint32x4_t fZero = vdupq_n_f32(0);
+    uint32x4_t uZero = vdupq_n_u32(0);
 
     for (int i = 0; i < FRAMELENGTH; i += 4) {
       float32x4_t l = vld1q_f32(left + i);
       float32x4_t r = vld1q_f32(right + i);
 
+      uint32x4_t lc = vcgtq_f32(l, fZero);
+      uint32x4_t rc = vcgtq_f32(r, fZero);
+
       // (l + r) > 0
-      float32x4_t m = vaddq_f32(l, r);
-      vst1q_f32(out + i, vcvtq_n_f32_u32(vcgtq_f32(m, zero), 32));
+      uint32x4_t _or = vcgtq_u32(vaddq_u32(lc, rc), uZero);
+      vst1q_f32(out + i, vcvtq_n_f32_u32(_or, 32));
     }
   }
 }
