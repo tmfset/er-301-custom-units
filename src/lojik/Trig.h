b6c62b8f3b3192b508ff5fdb6c19c1719a9518b0
Tom Fiset <tom.fiset@gemini.com>
2021-06-04 20:06:43 -0400

    Gate optimizations

--- src/lojik/Trig.h
+++ src/lojik/Trig.h
@@ -4,6 +4,7 @@
 #include <hal/simd.h>
 #include <od/objects/Object.h>
 #include <sense.h>
+#include <util.h>
 
 namespace lojik {
   class Trig : public od::Object {
@@ -11,7 +12,6 @@ namespace lojik {
       Trig() {
         addInput(mIn);
         addOutput(mOut);
-
         addOption(mSense);
       }
 
@@ -21,40 +21,26 @@ namespace lojik {
       virtual void process();
 
       void processInternal() {
-        float *in  = mIn.buffer();
+        const float *in  = mIn.buffer();
         float *out = mOut.buffer();
 
-        float32x4_t sense = vdupq_n_f32(getSense(mSense));
+        const auto sense = getSense(mSense);
 
         for (int i = 0; i < FRAMELENGTH; i += 4) {
-          float32x4_t v = vld1q_f32(in + i);
-
-          uint32_t vc[4];
-          vst1q_u32(vc, vcgtq_f32(v, sense));
-
-          for (int j = 0; j < 4; j++) {
-            if (!vc[j]) {
-              mAllow = true;
-            }
-
-            if (vc[j] && mAllow) {
-              mAllow = false;
-              out[i + j] = 1.0f;
-            } else {
-              out[i + j] = 0.0f;
-            }
-          }
+          uint32_t trig[4];
+          mLatch.readTriggersU(trig, vcgtq_f32(vld1q_f32(in + i), vdupq_n_f32(sense)));
+          vst1q_f32(out + i, vcvtq_n_f32_u32(vld1q_u32(trig), 32));
         }
       }
 
-      od::Inlet  mIn  { "In" };
-      od::Outlet mOut { "Out" };
-
+      od::Inlet  mIn    { "In" };
+      od::Outlet mOut   { "Out" };
       od::Option mSense { "Sense", INPUT_SENSE_LOW };
 #endif
 
     private:
       float mCurrent = 0.0f;
       bool mAllow = true;
+      Trigger mLatch;
   };
 }
