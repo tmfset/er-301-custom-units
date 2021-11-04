6acf50877ee9885bffe0d7dcc980b7184d76c17c
Tom Fiset <tom.fiset@gemini.com>
2021-11-04 00:04:24 -0400

    Remove curl

--- src/mods/curl/init.lua
+++
@@ -1,9 +0,0 @@
-return {
-  title   = "Curl",
-  contact = "tomj.fiset@gmail.com",
-  keyword = "wave folder",
-  units   = {
-    { category = "Mapping and Control" },
-    { title = "Curl", moduleName = "Curl", keywords = "wave folder" }
-  }
-}
