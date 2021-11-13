74b24f0c808ccaf20c5d37b4ed9411ee1c14a4e4
Tom Fiset <tom.fiset@gemini.com>
2021-11-13 17:43:48 -0500

    Capitals

--- scripts/mod-builder.mk
+++ scripts/mod-builder.mk
@@ -3,29 +3,6 @@ include scripts/utils.mk
 LIBNAME ?= lib$(PKGNAME)
 SDKPATH ?= er-301
 
-# Common code to all mods
-common_dir          = src/common
-common_assets_dir   = $(common_dir)/assets
-common_assets      := $(call rwildcard, $(common_assets_dir), *)
-common_c_sources   := $(call rwildcard, $(common_dir), *.c)
-common_cpp_sources := $(call rwildcard, $(common_dir), *.cpp)
-common_headers     := $(call rwildcard, $(common_dir), *.h)
-
-# Mod specific code
-mod_dir           = src/mods/$(PKGNAME)
-mod_assets_dir    = $(mod_dir)/assets
-mod_assets       := $(call rwildcard, $(mod_assets_dir), *)
-mod_c_sources    := $(call rwildcard, $(mod_dir), *.c)
-mod_cpp_sources  := $(call rwildcard, $(mod_dir), *.cpp)
-mod_swig_sources := $(call rwildcard, $(mod_dir), *.cpp.swig)
-mod_headers      := $(call rwildcard, $(mod_dir), *.h)
-
-swig_sources := $(mod_swig_sources)
-c_sources    := $(mod_c_sources) $(common_c_sources)
-cpp_sources  := $(mod_cpp_sources) $(common_cpp_sources)
-headers      := $(mod_headers) $(common_headers)
-assets       := $(mod_assets) $(common_assets)
-
 # Determine ARCH if it's not provided...
 # linux | darwin | am335x
 ifndef ARCH
@@ -39,25 +16,49 @@ ifndef ARCH
   endif
 endif
 
+
 # Determine PROFILE if it's not provided...
 # testing | release | debug
 PROFILE ?= testing
 
-out_dir      = $(PROFILE)/$(ARCH)
-lib_file     = $(out_dir)/$(LIBNAME).so
-package_dir  = $(out_dir)/$(PKGNAME)-$(PKGVERSION)
-package_file = $(package_dir).pkg
+OUT_DIR      = $(PROFILE)/$(ARCH)
+LIB_FILE     = $(OUT_DIR)/$(LIBNAME).so
+PACKAGE_DIR  = $(OUT_DIR)/$(PKGNAME)-$(PKGVERSION)
+PACKAGE_FILE = $(PACKAGE_DIR).pkg
 
-swig_wrapper = $(addprefix $(out_dir)/,$(swig_sources:%.cpp.swig=%_swig.cpp))
-swig_object  = $(swig_wrapper:%.cpp=%.o)
+# Common code to all mods
+COMMON_DIR          = src/common
+COMMON_ASSETS_DIR   = $(COMMON_DIR)/ASSETS
+COMMON_ASSETS      := $(call rwildcard, $(COMMON_ASSETS_DIR), *)
+COMMON_C_SOURCES   := $(call rwildcard, $(COMMON_DIR), *.c)
+COMMON_CPP_SOURCES := $(call rwildcard, $(COMMON_DIR), *.cpp)
+COMMON_HEADERS     := $(call rwildcard, $(COMMON_DIR), *.h)
 
-assembly  = $(addprefix $(out_dir)/,$(c_sources:%.c=%.s))
-assembly += $(addprefix $(out_dir)/,$(cpp_sources:%.cpp=%.s))
-assembly += $(swig_wrapper:%.cpp=%.s)
+# Mod specific code
+MOD_DIR           = src/mods/$(PKGNAME)
+MOD_ASSETS_DIR    = $(MOD_DIR)/ASSETS
+MOD_ASSETS       := $(call rwildcard, $(MOD_ASSETS_DIR), *)
+MOD_C_SOURCES    := $(call rwildcard, $(MOD_DIR), *.c)
+MOD_CPP_SOURCES  := $(call rwildcard, $(MOD_DIR), *.cpp)
+MOD_SWIG_SOURCES := $(call rwildcard, $(MOD_DIR), *.cpp.swig)
+MOD_HEADERS      := $(call rwildcard, $(MOD_DIR), *.h)
 
-objects  = $(addprefix $(out_dir)/,$(c_sources:%.c=%.o))
-objects += $(addprefix $(out_dir)/,$(cpp_sources:%.cpp=%.o))
-objects += $(swig_object)
+SWIG_SOURCES := $(MOD_SWIG_SOURCES)
+C_SOURCES    := $(MOD_C_SOURCES) $(COMMON_C_SOURCES)
+CPP_SOURCES  := $(MOD_CPP_SOURCES) $(COMMON_CPP_SOURCES)
+HEADERS      := $(MOD_HEADERS) $(COMMON_HEADERS)
+ASSETS       := $(MOD_ASSETS) $(COMMON_ASSETS)
+
+SWIG_WRAPPER = $(addprefix $(OUT_DIR)/,$(SWIG_SOURCES:%.cpp.swig=%_swig.cpp))
+SWIG_OBJECT  = $(SWIG_WRAPPER:%.cpp=%.o)
+
+ASSEMBLY  = $(addprefix $(OUT_DIR)/,$(C_SOURCES:%.c=%.s))
+ASSEMBLY += $(addprefix $(OUT_DIR)/,$(CPP_SOURCES:%.cpp=%.s))
+ASSEMBLY += $(SWIG_WRAPPER:%.cpp=%.s)
+
+OBJECTS  = $(addprefix $(OUT_DIR)/,$(C_SOURCES:%.c=%.o))
+OBJECTS += $(addprefix $(OUT_DIR)/,$(CPP_SOURCES:%.cpp=%.o))
+OBJECTS += $(SWIG_OBJECT)
 
 ifeq ($(ARCH),am335x)
   CFLAGS.am335x = -mcpu=cortex-a8 -mfpu=neon -mfloat-abi=hard -mabi=aapcs -Dfar= -D__DYNAMIC_REENT__
@@ -89,129 +90,129 @@ CFLAGS.release = $(CFLAGS.speed) -Wno-unused
 CFLAGS.testing = $(CFLAGS.speed) -DBUILDOPT_TESTING
 CFLAGS.debug   = -g -DBUILDOPT_TESTING
 
-# Do you need any additional preprocess symbols?
-symbols =
+# Do you need any additional preprocess SYMBOLS?
+SYMBOLS =
 
-includes  = $(mod_dir) $(common_dir)
-includes += $(SDKPATH) $(SDKPATH)/arch/$(ARCH) $(SDKPATH)/emu
+INCLUDES  = $(MOD_DIR) $(COMMON_DIR)
+INCLUDES += $(SDKPATH) $(SDKPATH)/arch/$(ARCH) $(SDKPATH)/emu
 
 CFLAGS += $(CFLAGS.common) $(CFLAGS.$(ARCH)) $(CFLAGS.$(PROFILE))
-CFLAGS += $(addprefix -I,$(includes))
-CFLAGS += $(addprefix -D,$(symbols))
+CFLAGS += $(addprefix -I,$(INCLUDES))
+CFLAGS += $(addprefix -D,$(SYMBOLS))
 
 # swig-specific flags
 SWIGFLAGS    = -lua -no-old-metatable-bindings -nomoduleglobal -small -fvirtual
-SWIGFLAGS   += $(addprefix -I,$(includes)) 
+SWIGFLAGS   += $(addprefix -I,$(INCLUDES)) 
 CFLAGS.swig  = $(CFLAGS.common) $(CFLAGS.$(ARCH)) $(CFLAGS.size)
-CFLAGS.swig += $(addprefix -I,$(includes)) -I$(SDKPATH)/libs/lua54
-CFLAGS.swig += $(addprefix -D,$(symbols))
+CFLAGS.swig += $(addprefix -I,$(INCLUDES)) -I$(SDKPATH)/libs/lua54
+CFLAGS.swig += $(addprefix -D,$(SYMBOLS))
 
 #######################################################
 # Rules
 
-all: $(package_file)
+all: $(PACKAGE_FILE)
 
-asm: $(assembly)
+asm: $(ASSEMBLY)
 
-$(swig_wrapper): $(headers) Makefile
+$(SWIG_WRAPPER): $(HEADERS) Makefile
 
-$(objects): $(headers) Makefile
+$(OBJECTS): $(HEADERS) Makefile
 
-$(assembly): $(headers) Makefile
+$(ASSEMBLY): $(HEADERS) Makefile
 
-$(lib_file): $(objects)
+$(LIB_FILE): $(OBJECTS)
 	@echo [LINK $@]
-	@$(CC) $(CFLAGS) -o $@ $(objects) $(LFLAGS)
+	@$(CC) $(CFLAGS) -o $@ $(OBJECTS) $(LFLAGS)
 
-$(package_dir): $(lib_file) $(assets)
+$(PACKAGE_DIR): $(LIB_FILE) $(ASSETS)
 	@echo [STAGE $@]
 	@rm -fr $@
 	@mkdir -p $@
-	@cp $(lib_file) $@/
-	@rsync -ru $(mod_assets_dir)/ $@/
-	@rsync -ru $(common_assets_dir)/ $@/
+	@cp $(LIB_FILE) $@/
+	@rsync -ru $(MOD_ASSETS_DIR)/ $@/
+	@rsync -ru $(COMMON_ASSETS_DIR)/ $@/
 	@find $@ -type f -name "*.lua" -print0 | xargs -0 sed -i.bak 's/common\.assets/$(PKGNAME)/g'
 	@find $@ -type f -name "*.bak" -print0 | xargs -0 rm
 
-.PHONY: $(package_dir)
+.PHONY: $(PACKAGE_DIR)
 
-$(package_file): $(package_dir)
+$(PACKAGE_FILE): $(PACKAGE_DIR)
 	@echo [ZIP $@]
 	@rm -f $@
 	@pwd
 	@cd $<; $(ZIP) -r ../$(@F) ./
 
-list: $(package_file)
-	@unzip -l $(package_file)
+list: $(PACKAGE_FILE)
+	@unzip -l $(PACKAGE_FILE)
 
 clean:
-	rm -rf $(out_dir)
+	rm -rf $(OUT_DIR)
 
 dist-clean:
 	rm -rf testing release debug
 
-install: $(package_file)
-	cp $(package_file) $(INSTALLROOT.$(ARCH))/ER-301/packages/
+install: $(PACKAGE_FILE)
+	cp $(PACKAGE_FILE) $(INSTALLROOT.$(ARCH))/ER-301/packages/
 
 install-sd:
-	cp $(package_file) /Volumes/NO\ Name/ER-301/packages/
+	cp $(PACKAGE_FILE) /Volumes/NO\ Name/ER-301/packages/
 
 # C/C++ compilation rules
 
 #  -fverbose-asm -fno-toplevel-reorder
-$(out_dir)/%.s: %.cpp
+$(OUT_DIR)/%.s: %.cpp
 	@echo [S $<]
 	@mkdir -p $(@D)
 	@$(CPP) $(CFLAGS) -w -std=gnu++11 -S $< -o $@
 
-$(out_dir)/%.o: %.c
+$(OUT_DIR)/%.o: %.c
 	@echo [C $<]
 	@mkdir -p $(@D)
 	@$(CC) $(CFLAGS) -std=gnu11 -c $< -o $@
 
-$(out_dir)/%.o: %.cpp
+$(OUT_DIR)/%.o: %.cpp
 	@echo [C++ $<]
 	@mkdir -p $(@D)
 	@$(CPP) $(CFLAGS) -std=gnu++11 -c $< -o $@
 
 # SWIG wrapper rules
 
-.PRECIOUS: $(out_dir)/%_swig.c $(out_dir)/%_swig.cpp
+.PRECIOUS: $(OUT_DIR)/%_swig.c $(OUT_DIR)/%_swig.cpp
 
-$(out_dir)/%_swig.cpp: %.cpp.swig
+$(OUT_DIR)/%_swig.cpp: %.cpp.swig
 	@echo [SWIG $<]
 	@mkdir -p $(@D)
 	@$(SWIG) -fvirtual -fcompact -c++ $(SWIGFLAGS) -o $@ $<
 
-$(out_dir)/%_swig.o: $(out_dir)/%_swig.cpp
+$(OUT_DIR)/%_swig.o: $(OUT_DIR)/%_swig.cpp
 	@echo [C++ $<]
 	@mkdir -p $(@D)
 	@$(CPP) $(CFLAGS.swig) -std=gnu++11 -c $< -o $@
 
 ifeq ($(ARCH),am335x)
-# Optional check for any missing symbols
+# Optional check for any missing SYMBOLS
 #
 # make missing ARCH=am335x
 #
 # Warning: This assumes that the firmware has been compiled in the SDK source tree already. 
 #
 
-all_imports_file     = $(out_dir)/imports.txt
-missing_imports_file = $(out_dir)/missing.txt
-app_exports_file     = $(SDKPATH)/$(PROFILE)/$(ARCH)/app/exports.sym
+ALL_IMPORTS_FILE     = $(OUT_DIR)/imports.txt
+MISSING_IMPORTS_FILE = $(OUT_DIR)/missing.txt
+APP_EXPORTS_FILE     = $(SDKPATH)/$(PROFILE)/$(ARCH)/app/exports.sym
 
-$(all_imports_file): $(lib_file)
+$(ALL_IMPORTS_FILE): $(LIB_FILE)
 	@echo $(describe_env) NM $<
 	@$(NM) --undefined-only --format=posix $< | awk '{print $$1;}' > $@
 
-$(missing_imports_file): $(all_imports_file) $(app_exports_file)
-	@sort -u $(app_exports_file) | comm -23 $< - > $@
+$(MISSING_IMPORTS_FILE): $(ALL_IMPORTS_FILE) $(APP_EXPORTS_FILE)
+	@sort -u $(APP_EXPORTS_FILE) | comm -23 $< - > $@
 
-# Check for any undefined symbols that are not exported by app.
-missing: $(missing_imports_file)
+# Check for any undefined SYMBOLS that are not exported by app.
+missing: $(MISSING_IMPORTS_FILE)
 	@[ ! -s $< ] || echo "Missing Symbols:"
 	@cat $<
-	@[ -s $< ] || echo "No missing symbols."	
+	@[ -s $< ] || echo "No missing SYMBOLS."	
 
 .PHONY: missing
 
